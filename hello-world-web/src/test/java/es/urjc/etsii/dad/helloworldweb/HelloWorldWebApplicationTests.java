92da2402bbb43ef7f5f0e4595662c20690dc431e
JorgeM1404 <molinatiradoj@gmail.com>
2021-02-15 14:17:11 +0100

    Se ha subido un proyecto base

---
+++ hello-world-web/src/test/java/es/urjc/etsii/dad/helloworldweb/HelloWorldWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.etsii.dad.helloworldweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
