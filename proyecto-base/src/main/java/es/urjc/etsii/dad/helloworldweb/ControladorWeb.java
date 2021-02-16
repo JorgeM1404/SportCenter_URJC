230294cda1a918a876eee896901ecc45479b023a
JorgeM1404 <molinatiradoj@gmail.com>
2021-02-16 12:44:17 +0100

    Se ha actualizado el proyecto base

---
+++ proyecto-base/src/main/java/es/urjc/etsii/dad/helloworldweb/ControladorWeb.java
@@ -0,0 +1,28 @@
+package es.urjc.etsii.dad.helloworldweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ControladorWeb 
+{
+	@GetMapping("/hola-mundo")
+	public String holaMundo()
+	{
+		return "hola_mundo.html";
+	}
+	
+	@GetMapping("/")
+	public String saludo()
+	{
+		return "saludo.html";
+	}
+	
+	@RequestMapping("/hola")
+	public String hola(Model model, @RequestParam String userName)
+	{
+		model.addAttribute("nombre", userName);
+		return "hola_template";
+	}
+}
