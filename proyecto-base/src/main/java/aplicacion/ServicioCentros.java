4f8c90b393a3b6e4d8c8a865028bdde00ba974a9
aroldan <af.roldan@alumnos.urjc.es>
2021-03-04 19:45:28 +0100

    Changes in ServicioCentros to add all activities in all centers.

--- proyecto-base/src/main/java/aplicacion/ServicioCentros.java
+++ proyecto-base/src/main/java/aplicacion/ServicioCentros.java
@@ -24,25 +24,100 @@ public class ServicioCentros
 	    repositorio.save(centro1);
 	    setActividadesMostoles(centro1);
 	   
-	    repositorio.save(new CentroDeportivo ("Alcorcón"));
-	    repositorio.save(new CentroDeportivo ("Fuenlabrada"));
-	    repositorio.save(new CentroDeportivo ("Aranjuez"));
-	    repositorio.save(new CentroDeportivo ("Vicálvaro"));
+	    CentroDeportivo centro2 = new CentroDeportivo("Alcorcón");		
+	    repositorio.save(centro2);
+	    setActividadesAlcorcon(centro2);
+	    
+	    CentroDeportivo centro3 = new CentroDeportivo("Fuenlabrada");		
+	    repositorio.save(centro3);
+	    setActividadesFuenlabrada(centro3);
+	    
+	    CentroDeportivo centro4 = new CentroDeportivo("Aranjuez");		
+	    repositorio.save(centro4);
+	    setActividadesAranjuez(centro4);
+	    
+	    CentroDeportivo centro5 = new CentroDeportivo("Vicálvaro");		
+	    repositorio.save(centro5);
+	    setActividadesVicalvaro(centro5);
+	    
+	    
+	    //repositorio.save(new CentroDeportivo ("Alcorcón"));
+	    //repositorio.save(new CentroDeportivo ("Fuenlabrada"));
+	    //repositorio.save(new CentroDeportivo ("Aranjuez"));
+	    //repositorio.save(new CentroDeportivo ("Vicálvaro"));
 	}
-	
+		
 	public void setActividadesMostoles(CentroDeportivo centro)
 	{		
-		Actividad act1 = new Actividad("Football");  act1.getCentros().add(centro);
-		Actividad act2 = new Actividad("Baloncesto");  act2.getCentros().add(centro);
-		Actividad act3 = new Actividad("Tenis");  act3.getCentros().add(centro);
-		Actividad act4 = new Actividad("Balonmano");  act4.getCentros().add(centro);
-		Actividad act5 = new Actividad("Natacion");  act5.getCentros().add(centro);
+		Actividad act1 = new Actividad("Football_Mostoles");  act1.getCentros().add(centro);
+		Actividad act2 = new Actividad("Baloncesto_Mostoles");  act2.getCentros().add(centro);
+		Actividad act3 = new Actividad("Tenis_Mostoles");  act3.getCentros().add(centro);
+		Actividad act4 = new Actividad("Natacion_Mostoles");  act4.getCentros().add(centro);
+		Actividad act5 = new Actividad("Judo_Mostoles");  act5.getCentros().add(centro);
+		Actividad act6 = new Actividad("Padel_Mostoles");  act6.getCentros().add(centro);
+		
+		servicioActividades.guardarActividad(act1);
+		servicioActividades.guardarActividad(act2);
+		servicioActividades.guardarActividad(act3);
+		servicioActividades.guardarActividad(act4);
+		servicioActividades.guardarActividad(act5);
+		servicioActividades.guardarActividad(act6);
+	}
+	
+	public void setActividadesAlcorcon(CentroDeportivo centro)
+	{		
+		Actividad act1 = new Actividad("Football_Alcorcon");  act1.getCentros().add(centro);
+		Actividad act2 = new Actividad("Baloncesto_Alcorcon");  act2.getCentros().add(centro);
+		Actividad act3 = new Actividad("Balonmano_Alcorcon");  act3.getCentros().add(centro);
+		Actividad act4 = new Actividad("Judo_Alcorcon");  act4.getCentros().add(centro);
+		Actividad act5 = new Actividad("Padel_Alcorcon");  act5.getCentros().add(centro);
 
 		servicioActividades.guardarActividad(act1);
 		servicioActividades.guardarActividad(act2);
 		servicioActividades.guardarActividad(act3);
 		servicioActividades.guardarActividad(act4);
 		servicioActividades.guardarActividad(act5);
+	}
+	
+	public void setActividadesFuenlabrada(CentroDeportivo centro)
+	{		
+		Actividad act1 = new Actividad("Football_Fuenla");  act1.getCentros().add(centro);
+		Actividad act2 = new Actividad("Baloncesto_Fuenla");  act2.getCentros().add(centro);
+		Actividad act3 = new Actividad("Tenis_Fuenla");  act3.getCentros().add(centro);
+		Actividad act4 = new Actividad("Balonmano_Fuenla");  act4.getCentros().add(centro);
+		Actividad act5 = new Actividad("Hockey_Fuenla");  act5.getCentros().add(centro);
+		Actividad act6 = new Actividad("Judo_Fuenla");  act6.getCentros().add(centro);
+		
+		servicioActividades.guardarActividad(act1);
+		servicioActividades.guardarActividad(act2);
+		servicioActividades.guardarActividad(act3);
+		servicioActividades.guardarActividad(act4);
+		servicioActividades.guardarActividad(act5);
+		servicioActividades.guardarActividad(act6);
+	}
+	
+	public void setActividadesAranjuez(CentroDeportivo centro)
+	{		
+		Actividad act1 = new Actividad("Football_Aranjuez");  act1.getCentros().add(centro);
+		Actividad act2 = new Actividad("Baloncesto_Aranjuez");  act2.getCentros().add(centro);
+		Actividad act3 = new Actividad("Tenis_Aranjuez");  act3.getCentros().add(centro);
+		Actividad act4 = new Actividad("Natacion_Aranjuez");  act4.getCentros().add(centro);
+		
+		servicioActividades.guardarActividad(act1);
+		servicioActividades.guardarActividad(act2);
+		servicioActividades.guardarActividad(act3);
+		servicioActividades.guardarActividad(act4);
+	}
+	
+	public void setActividadesVicalvaro(CentroDeportivo centro)
+	{		
+		Actividad act1 = new Actividad("Football_Vicalvaro");  act1.getCentros().add(centro);
+		Actividad act2 = new Actividad("Judo_Vicalvaro");  act2.getCentros().add(centro);
+		Actividad act3 = new Actividad("Padel_Vicalvaro");  act3.getCentros().add(centro);
+
+		servicioActividades.guardarActividad(act1);
+		servicioActividades.guardarActividad(act2);
+		servicioActividades.guardarActividad(act3);
 	}
 	
 	public List<Actividad> getActividadesCentro(String id) {
