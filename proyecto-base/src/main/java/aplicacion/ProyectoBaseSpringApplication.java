4f4d1728a5a2a2ce20d50dee479f9ab091a0df5a
afroldan <af.roldan@alumnos.urjc.es>
2021-05-03 19:47:01 +0200

    Añadidas dependencias Spring Session & Hazelcast

--- proyecto-base/src/main/java/aplicacion/ProyectoBaseSpringApplication.java
+++ proyecto-base/src/main/java/aplicacion/ProyectoBaseSpringApplication.java
@@ -6,9 +6,16 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+
+import java.util.Collections;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+@EnableHazelcastHttpSession
 @EnableCaching
 @SpringBootApplication
 public class ProyectoBaseSpringApplication 
@@ -18,6 +25,18 @@ public class ProyectoBaseSpringApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(ProyectoBaseSpringApplication.class, args);
+	}
+	
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+	
+		return config;
 	}
 	
 	@Bean
